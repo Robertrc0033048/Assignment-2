878518142f1a630f33fc892ff8cf71d83e1f29e0
Robertrc0033048 <rc0033048@student.riversidecollege.ac.uk>
2019-06-04 10:06:38 +0100

    added parallel

--- Assignment 2/Form1.Designer.cs
+++ Assignment 2/Form1.Designer.cs
@@ -28,13 +28,336 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.VAL1 = new System.Windows.Forms.Label();
+            this.VAL2 = new System.Windows.Forms.Label();
+            this.VAL3 = new System.Windows.Forms.Label();
+            this.VAL4 = new System.Windows.Forms.Label();
+            this.VAL5 = new System.Windows.Forms.Label();
+            this.SeriesOUT = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox23 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ParallelOUT = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(105, 98);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(696, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.SeriesOUT);
+            this.tabPage1.Controls.Add(this.VAL5);
+            this.tabPage1.Controls.Add(this.VAL4);
+            this.tabPage1.Controls.Add(this.VAL3);
+            this.tabPage1.Controls.Add(this.VAL2);
+            this.tabPage1.Controls.Add(this.VAL1);
+            this.tabPage1.Controls.Add(this.textBox6);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(688, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Series";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(142, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(142, 153);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(142, 257);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // VAL1
+            // 
+            this.VAL1.AutoSize = true;
+            this.VAL1.Location = new System.Drawing.Point(76, 55);
+            this.VAL1.Name = "VAL1";
+            this.VAL1.Size = new System.Drawing.Size(43, 13);
+            this.VAL1.TabIndex = 6;
+            this.VAL1.Text = "Value 1";
+            // 
+            // VAL2
+            // 
+            this.VAL2.AutoSize = true;
+            this.VAL2.Location = new System.Drawing.Point(76, 108);
+            this.VAL2.Name = "VAL2";
+            this.VAL2.Size = new System.Drawing.Size(43, 13);
+            this.VAL2.TabIndex = 7;
+            this.VAL2.Text = "Value 2";
+            // 
+            // VAL3
+            // 
+            this.VAL3.AutoSize = true;
+            this.VAL3.Location = new System.Drawing.Point(76, 160);
+            this.VAL3.Name = "VAL3";
+            this.VAL3.Size = new System.Drawing.Size(43, 13);
+            this.VAL3.TabIndex = 8;
+            this.VAL3.Text = "Value 3";
+            // 
+            // VAL4
+            // 
+            this.VAL4.AutoSize = true;
+            this.VAL4.Location = new System.Drawing.Point(76, 212);
+            this.VAL4.Name = "VAL4";
+            this.VAL4.Size = new System.Drawing.Size(43, 13);
+            this.VAL4.TabIndex = 9;
+            this.VAL4.Text = "Value 4";
+            // 
+            // VAL5
+            // 
+            this.VAL5.AutoSize = true;
+            this.VAL5.Location = new System.Drawing.Point(76, 264);
+            this.VAL5.Name = "VAL5";
+            this.VAL5.Size = new System.Drawing.Size(43, 13);
+            this.VAL5.TabIndex = 10;
+            this.VAL5.Text = "Value 5";
+            // 
+            // SeriesOUT
+            // 
+            this.SeriesOUT.AutoSize = true;
+            this.SeriesOUT.Location = new System.Drawing.Point(368, 56);
+            this.SeriesOUT.Name = "SeriesOUT";
+            this.SeriesOUT.Size = new System.Drawing.Size(110, 13);
+            this.SeriesOUT.TabIndex = 11;
+            this.SeriesOUT.Text = "Resistance in series =";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.ParallelOUT);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.textBox22);
+            this.tabPage2.Controls.Add(this.textBox23);
+            this.tabPage2.Controls.Add(this.textBox21);
+            this.tabPage2.Controls.Add(this.textBox20);
+            this.tabPage2.Controls.Add(this.textBox19);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(688, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Parallel";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(146, 61);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(100, 20);
+            this.textBox19.TabIndex = 0;
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(146, 115);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(100, 20);
+            this.textBox20.TabIndex = 1;
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(146, 169);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(100, 20);
+            this.textBox21.TabIndex = 2;
+            // 
+            // textBox23
+            // 
+            this.textBox23.Location = new System.Drawing.Point(146, 274);
+            this.textBox23.Name = "textBox23";
+            this.textBox23.Size = new System.Drawing.Size(100, 20);
+            this.textBox23.TabIndex = 3;
+            // 
+            // textBox22
+            // 
+            this.textBox22.Location = new System.Drawing.Point(146, 221);
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(100, 20);
+            this.textBox22.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Value 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Value 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Value 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Value 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Value 5";
+            // 
+            // ParallelOUT
+            // 
+            this.ParallelOUT.AutoSize = true;
+            this.ParallelOUT.Location = new System.Drawing.Point(377, 67);
+            this.ParallelOUT.Name = "ParallelOUT";
+            this.ParallelOUT.Size = new System.Drawing.Size(117, 13);
+            this.ParallelOUT.TabIndex = 10;
+            this.ParallelOUT.Text = "Resistance in Parallel =";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(937, 571);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label SeriesOUT;
+        private System.Windows.Forms.Label VAL5;
+        private System.Windows.Forms.Label VAL4;
+        private System.Windows.Forms.Label VAL3;
+        private System.Windows.Forms.Label VAL2;
+        private System.Windows.Forms.Label VAL1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox23;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label ParallelOUT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,49 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Value1, Value2, Value3, Value4, Value5, RT;
+
+                Value1 = Convert.ToDouble(textBox1.Text);
+                Value2 = Convert.ToDouble(textBox2.Text);
+                Value3 = Convert.ToDouble(textBox5.Text);
+                Value4 = Convert.ToDouble(textBox4.Text);
+                Value5 = Convert.ToDouble(textBox6.Text);
+
+                RT = Value1 + Value2 + Value3 + Value4 + Value5;
+
+                SeriesOUT.Text = "Resistance in Series =" + RT;
+            }
+            catch
+            {
+                MessageBox.Show("please input all values.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Value1, Value2, Value3, Value4, Value5, RT;
+
+                Value1 = Convert.ToDouble(textBox19.Text);
+                Value2 = Convert.ToDouble(textBox20.Text);
+                Value3 = Convert.ToDouble(textBox21.Text);
+                Value4 = Convert.ToDouble(textBox22.Text);
+                Value5 = Convert.ToDouble(textBox23.Text);
+
+                RT = 1 / (1 / Value1 + 1 / Value2 + 1 / Value3 + 1 / Value4 + 1 / Value5);
+
+                ParallelOUT.Text = "Resistance in series =" + RT;
+            }
+            catch
+            {
+                MessageBox.Show("Please input all values");
+            }
+        }
     }
 }
